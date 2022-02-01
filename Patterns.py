40185eb766a2fd4f03967f2513900da8e8a0f0ec
Alan <96013927+somerandomguynocap@users.noreply.github.com>
2022-02-01 19:15:30 +0400

    Add files via upload

---
+++ Patterns.py
@@ -0,0 +1,11 @@
+# MADE BY ALAN
+for j in range(5):    
+    for i in range(j):
+        print("0 ", end="")
+    print()
+for f in range(5):    
+    for g in range(4-f):
+        print("0 ", end="")
+    print()
+
+input()
